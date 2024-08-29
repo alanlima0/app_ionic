d72d3deed7b61f1011cd68f33b20d8e41a42828d
alanlima0 <88351930+alanlima0@users.noreply.github.com>
2024-08-29 15:42:59 -0300

    Add files via upload

---
+++ src/pages/home/teste.ts
@@ -0,0 +1,100 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  pacientes = [];
+
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {}
+
+  showPrompt() {
+    const prompt = this.alertCtrl.create({
+      title: 'Adicionar Paciente',
+      message: "Insira os dados:",
+      inputs: [
+        {
+          name: 'nome',
+          placeholder: 'Nome'
+        },
+        {
+          name: 'data_nasc',
+          placeholder: 'Data de Nascimento'
+        },
+        {
+          name: 'cpf',
+          placeholder: 'CPF'
+        },
+        {
+          name: 'telefone',
+          placeholder: 'Telefone'
+        },
+        {
+          name: 'endereco',
+          placeholder: 'Endereço'
+        },
+        {
+          name: 'foto',
+          placeholder: 'URL da foto'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            console.log('Saved clicked');
+            let a = { nome: data.nome, data_nasc: data.data_nasc, telefone: data.telefone, endereco: data.endereco , cpf: data.cpf , foto:data.foto};
+            this.pacientes.push(a); // Adiciona a nova nota ao array
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+  excluir(item) {
+    console.log('Excluir', item);
+
+    for (let i = 0; i < this.pacientes.length; i++) {
+      const element = this.pacientes[i];
+      
+      if (element.nome === item.nome) {
+        this.pacientes.splice(i, 1); // Remove a nota do array
+        break; // Sai do loop após encontrar e remover a nota
+      }
+    }
+  }
+
+  showConfirm(item) {
+    const confirm = this.alertCtrl.create({
+      title: 'Excluir',
+      message: 'Tem certeza que deseja excluir?',
+      buttons: [
+        {
+          text: 'Não',
+          handler: () => {
+            console.log('Disagree clicked');
+          }
+        },
+        {
+          text: 'Sim',
+          handler: () => {
+            console.log('Agree clicked');
+            this.excluir(item); // Chama o método de exclusão
+          }
+        }
+      ]
+    });
+    confirm.present();
+  }
+}
